161ee6fbe7e7bee9250657999a3229f13ea29c13
Eric Nelson <e82.eric@gmail.com>
2015-01-03 00:25:22 -0800

    added conemu settings file

--- make_links.cmd
+++ make_links.cmd
@@ -3,5 +3,9 @@ IF EXIST %vimrc% del /F %vimrc%
  
 mklink %vimrc% "%~dp0vimfiles\.vimrc"
 
+SET conemu="%appdata%\conemu.xml"
+IF EXIST %conemu% del /F %conemu%
+mklink %conemu% "%~dp0conemu.xml"
+
 reg add HKEY_CURRENT_USER\Software\Microsoft\visualstudio\12.0\ImportExportSettings\UserPreferences /f /v ImportBrowsePath /d "%~dp0" /t reg_sz
 "c:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe" /ResetSettings "%~dp0CurrentSettings.vssettings"
