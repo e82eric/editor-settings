6b0671270082d994c8caea5793b7f5e3894f5988
Eric Nelson <e82.eric@gmail.com>
2015-01-02 21:57:10 -0800

    added script for creating links and updated settings file so hopefully visual studio will import it

---
+++ make_links.cmd
@@ -0,0 +1,2 @@
+if not exist "%userprofile%\Documents\visual studio 2013\settings" mkdir "%userprofile%\Documents\visual studio 2013\settings"
+mklink "%userprofile%\Documents\visual studio 2013\settings\CurrentSettings.vssettings" c:\editor-settings\CurrentSettings.vssettings
