cc01febce04a5fa423f4f3bb4306415a5ab1c579
Eric Nelson <e82.eric@gmail.com>
2015-01-03 00:06:17 -0800

    I think this will make visual studio work

--- make_links.cmd
+++ make_links.cmd
@@ -2,3 +2,6 @@ SET vimrc="%userprofile%\.vimrc"
 IF EXIST %vimrc% del /F %vimrc%
  
 mklink %vimrc% "%~dp0vimfiles\.vimrc"
+
+reg add HKEY_CURRENT_USER\Software\Microsoft\visualstudio\12.0\ImportExportSettings\UserPreferences /f /v ImportBrowsePath /d "%~dp0" /t reg_sz
+"c:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe" /ResetSettings "%~dp0CurrentSettings.vssettings"
